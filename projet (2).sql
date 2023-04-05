bf1e4be2f917c001496f26608bf329582c34d09a
Morneee <129755978+Morneee@users.noreply.github.com>
2023-04-05 18:15:40 +0200

    nouvelle bdd

--- projet (2).sql
+++ projet (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le : lun. 03 avr. 2023 à 18:15
+-- Généré le : mer. 05 avr. 2023 à 16:14
 -- Version du serveur : 5.7.24
 -- Version de PHP : 8.0.1
 
@@ -38,6 +38,14 @@ CREATE TABLE `article` (
   `date_modification` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Déchargement des données de la table `article`
+--
+
+INSERT INTO `article` (`id_article`, `titre`, `contenu`, `note`, `date_creation`, `jaquette`, `id_user`, `date_modification`) VALUES
+(1, 'Le monde de cube ou quoi là', 'BLABLABLAencore', 7, '2023-04-05', NULL, 2, NULL),
+(2, 'En route vers la 97', 'BLABLABLA', 10, '2023-04-05', NULL, 1, NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -51,7 +59,6 @@ CREATE TABLE `avis` (
   `note` int(11) DEFAULT NULL,
   `date_creation` date DEFAULT NULL,
   `id_jeux` int(11) DEFAULT NULL,
-  `note_moyenne` int(11) DEFAULT NULL,
   `id_user` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -62,9 +69,68 @@ CREATE TABLE `avis` (
 --
 
 CREATE TABLE `categories` (
-  `nom_categorie` varchar(50) NOT NULL,
+  `id_categorie` int(11) NOT NULL,
+  `nom_categorie` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `categories`
+--
+
+INSERT INTO `categories` (`id_categorie`, `nom_categorie`) VALUES
+(1, 'RPG'),
+(2, 'Openworld'),
+(3, 'Aventure'),
+(4, 'Sandbox'),
+(5, 'Narratif'),
+(6, 'Puzzle'),
+(7, 'Action');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `estcategories`
+--
+
+CREATE TABLE `estcategories` (
+  `id_categorie` int(11) DEFAULT NULL,
   `id_jeux` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `estcategories`
+--
+
+INSERT INTO `estcategories` (`id_categorie`, `id_jeux`) VALUES
+(2, 3),
+(4, 3),
+(7, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `estsupport`
+--
+
+CREATE TABLE `estsupport` (
+  `id_support` int(11) DEFAULT NULL,
+  `id_jeux` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `estsupport`
+--
+
+INSERT INTO `estsupport` (`id_support`, `id_jeux`) VALUES
+(1, 1),
+(5, 1),
+(4, 1),
+(5, 2),
+(1, 3),
+(2, 3),
+(5, 3),
+(8, 3),
+(9, 3);
 
 -- --------------------------------------------------------
 
@@ -93,6 +159,19 @@ CREATE TABLE `jeux` (
   `id_article` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Déchargement des données de la table `jeux`
+--
+
+INSERT INTO `jeux` (`id_jeux`, `nom`, `prix`, `date_sortie`, `synopsis`, `id_article`) VALUES
+(1, 'Road 96', 19.96, '2021-08-16', 'Jeu incroyable', 2),
+(2, 'Portal', 9.75, '2007-10-10', 'Jeu de portail', NULL),
+(3, 'Minecraft', 29.99, '2011-11-18', 'Jeu de cube', 1),
+(4, 'GTA V', 29.98, '2013-09-17', 'Jeu de bac a sable', NULL),
+(5, 'Halo', 39.99, '2001-11-15', 'Jeu d aventure', NULL),
+(6, 'KOTOR', 8.19, '2003-07-15', 'Aventure RPG jeux d aventure ', NULL),
+(7, 'BANANAPOCALYPSE', 46.99, '2023-01-10', 'Jeu de banane', NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -100,9 +179,24 @@ CREATE TABLE `jeux` (
 --
 
 CREATE TABLE `support` (
-  `nom_support` varchar(50) NOT NULL,
-  `id_jeux` int(11) DEFAULT NULL
+  `id_support` int(11) NOT NULL,
+  `nom_support` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `support`
+--
+
+INSERT INTO `support` (`id_support`, `nom_support`) VALUES
+(1, 'Xbox one'),
+(2, 'Xbox 360'),
+(3, 'Playstation 1'),
+(4, 'Nintendo Switch'),
+(5, 'Ordinateur'),
+(6, 'Playstation 2'),
+(7, 'Playstation 3'),
+(8, 'Playstation 4'),
+(9, 'Playstation 5');
 
 -- --------------------------------------------------------
 
@@ -120,8 +214,18 @@ CREATE TABLE `utilisateur` (
   `date_naissance` date DEFAULT NULL,
   `date_creation_compte` date DEFAULT NULL,
   `date_connexion` date DEFAULT NULL,
-  `id_image` int(11) DEFAULT NULL
+  `id_image` int(11) DEFAULT NULL,
+  `rôle` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `utilisateur`
+--
+
+INSERT INTO `utilisateur` (`id_user`, `login`, `mdp`, `nom`, `prenom`, `adresse_mail`, `date_naissance`, `date_creation_compte`, `date_connexion`, `id_image`, `rôle`) VALUES
+(1, 'laican', 'mdp', 'Vailland', 'Damien', 'damien.vailland@etudiant.univ-rennes1.fr', '2003-09-01', '2023-04-05', NULL, NULL, 'administrateur'),
+(2, 'mornee', 'mdp', 'Theault', 'Morgane', 'morgane.theault@etudiant.univ-rennes1.fr', '2003-05-14', '2023-04-05', NULL, NULL, 'redacteur'),
+(3, 'hyppo', 'mdp', 'Tribut', 'Hippolyte', 'hippolyte.tribut@etudiant.univ-rennes1.fr', '2003-12-18', '2023-04-05', NULL, NULL, 'Membre');
 
 --
 -- Index pour les tables déchargées
@@ -146,8 +250,21 @@ ALTER TABLE `avis`
 -- Index pour la table `categories`
 --
 ALTER TABLE `categories`
-  ADD PRIMARY KEY (`nom_categorie`),
-  ADD KEY `fk_id_jeux_cat` (`id_jeux`);
+  ADD PRIMARY KEY (`id_categorie`);
+
+--
+-- Index pour la table `estcategories`
+--
+ALTER TABLE `estcategories`
+  ADD KEY `fk_id_jeux_estcategories` (`id_categorie`),
+  ADD KEY `fk_id_jeu_estcategories` (`id_jeux`);
+
+--
+-- Index pour la table `estsupport`
+--
+ALTER TABLE `estsupport`
+  ADD KEY `id_jeux` (`id_jeux`),
+  ADD KEY `id_support` (`id_support`);
 
 --
 -- Index pour la table `images`
@@ -167,8 +284,7 @@ ALTER TABLE `jeux`
 -- Index pour la table `support`
 --
 ALTER TABLE `support`
-  ADD PRIMARY KEY (`nom_support`),
-  ADD KEY `fk_id_jeux` (`id_jeux`);
+  ADD PRIMARY KEY (`id_support`);
 
 --
 -- Index pour la table `utilisateur`
@@ -195,10 +311,18 @@ ALTER TABLE `avis`
   ADD CONSTRAINT `fk_id_user` FOREIGN KEY (`id_user`) REFERENCES `utilisateur` (`id_user`);
 
 --
--- Contraintes pour la table `categories`
+-- Contraintes pour la table `estcategories`
 --
-ALTER TABLE `categories`
-  ADD CONSTRAINT `fk_id_jeux_cat` FOREIGN KEY (`id_jeux`) REFERENCES `jeux` (`id_jeux`);
+ALTER TABLE `estcategories`
+  ADD CONSTRAINT `fk_id_jeu_estcategories` FOREIGN KEY (`id_jeux`) REFERENCES `jeux` (`id_jeux`),
+  ADD CONSTRAINT `fk_id_jeux_estcategories` FOREIGN KEY (`id_categorie`) REFERENCES `categories` (`id_categorie`);
+
+--
+-- Contraintes pour la table `estsupport`
+--
+ALTER TABLE `estsupport`
+  ADD CONSTRAINT `estsupport_ibfk_1` FOREIGN KEY (`id_jeux`) REFERENCES `jeux` (`id_jeux`),
+  ADD CONSTRAINT `estsupport_ibfk_2` FOREIGN KEY (`id_support`) REFERENCES `support` (`id_support`);
 
 --
 -- Contraintes pour la table `images`
@@ -211,12 +335,6 @@ ALTER TABLE `images`
 --
 ALTER TABLE `jeux`
   ADD CONSTRAINT `fk_id_article` FOREIGN KEY (`id_article`) REFERENCES `article` (`id_article`);
-
---
--- Contraintes pour la table `support`
---
-ALTER TABLE `support`
-  ADD CONSTRAINT `fk_id_jeux` FOREIGN KEY (`id_jeux`) REFERENCES `jeux` (`id_jeux`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
