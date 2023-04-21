660747844868fb2a24d6809065edb4d9e3eb5e14
Laïcan <116171756+Laicannn@users.noreply.github.com>
2023-04-21 12:12:17 +0200

    Retire animation sur meme page

--- static/header.php
+++ static/header.php
@@ -11,14 +11,27 @@
     </a>
     <?php
         if (isset($_SESSION['logged']) && $_SESSION['logged'] === true){
-            echo '<div class="double_button">';
-                echo'<a id="account_button" href="account.php"><img src="images/buttons/bouton_compte.png"></a>';
-                echo '<a id="disconnect_button" href="php/logout.php"><img src="images/buttons/bouton_disconnect.png"></a>';
-            echo'</div>';
+            if(isset($_SERVER['HTTP_REFERER']) && str_contains($_SERVER['HTTP_REFERER'],$_SERVER['PHP_SELF'])){
+                echo '<div class="double_button">';
+                    echo'<a id="account_button" href="account.php"><img src="images/buttons/bouton_compte.png"></a>';
+                    echo '<a id="disconnect_button" href="php/logout.php"><img src="images/buttons/bouton_disconnect.png"></a>';
+                echo'</div>';
+            } else {
+                echo '<div class="double_button_anime">';
+                    echo'<a id="account_button" href="account.php"><img src="images/buttons/bouton_compte.png"></a>';
+                    echo '<a id="disconnect_button" href="php/logout.php"><img src="images/buttons/bouton_disconnect.png"></a>';
+                echo'</div>';
+            }
+            
         }
         else{
             if (!(str_ends_with($_SERVER['REQUEST_URI'], 'connection.php')) && !(str_ends_with($_SERVER['REQUEST_URI'], '?msg=erreur'))){
-                echo '<a id="connection_button" href="connection.php"><img src="images/buttons/bouton_connect.png"></a>';
+                if(isset($_SERVER['HTTP_REFERER']) && str_contains($_SERVER['HTTP_REFERER'],$_SERVER['PHP_SELF'])){
+                    echo '<a id="connection_button" class="connection_button_class" href="connection.php"><img src="images/buttons/bouton_connect.png"></a>';
+                }
+                else {
+                    echo '<a id="connection_button_anime" class="connection_button_class" href="connection.php"><img src="images/buttons/bouton_connect.png"></a>';
+                }
             }
         }
     ?>
