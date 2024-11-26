5a21a64a1570d1f899df9b9b063b3eff026e7bf8
Laïcan <167437065+laican-fr@users.noreply.github.com>
2024-11-26 17:25:41 +0100

    avancement titre respnsive

--- includes/config-bdd.php
+++ includes/config-bdd.php
@@ -2,6 +2,6 @@
 //Paramètres de connexion à la base de données
 define('SERVEUR', "localhost");         //hote
 define('USER', "root");                 //login, par exemple "root" ou ""
-define('PWD', "root");                  //mot de passe, par exemple "root" ou ""
-define('DB_NAME', 'projet');  //nom de la base de données
+define('PWD', "");                  //mot de passe, par exemple "root" ou ""
+define('DB_NAME', 'mdtrikit');  //nom de la base de données
 ?>
